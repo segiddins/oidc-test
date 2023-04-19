15d32471f1ea7e6208c6aa5a38b796357eed8269
Samuel Giddins <segiddins@segiddins.me>
2023-04-20 00:09:26 +0200

    Push gem with OIDC

---
+++ spec/oidc/test_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Oidc::Test do
+  it "has a version number" do
+    expect(Oidc::Test::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
